04dd5ac326884d4e569a5d312ca15d1e9e96568d
Stefania <stt.orlova@gmail.com>
2021-10-21 17:00:31 +0300

    Homeworks

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,68 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++){
+    sum += arr[i];
 
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+
+  avg = parseFloat((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
   let sum;
-
-  // Ваш код
+  sum = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
   let max;
+  max = -Infinity;
 
-  // Ваш кода
-  // for ...
+  for (let j = 0; j < arrOfArr.length; j++) {
+
+    if (func(arrOfArr[j]) > max) {
+      max = func(arrOfArr[j]);
+    }
+
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let max = -Infinity;
+  let min = arr[0];
+
+  for (let i = 0; i < arr.length; i += 1) {
+
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+
+    if(arr[i] <=min) {
+      min = arr[i];
+    }
+  }
+
+  return (max -min);
 }
