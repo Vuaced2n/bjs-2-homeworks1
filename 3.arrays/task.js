a86881e38fde42f09a8ad791f7a38baee8949f5e
Stefania <stt.orlova@gmail.com>
2021-10-25 13:41:01 +0300

    homeworks

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,21 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  if (arr1.length != arr2.length) {
+    result = false;
+  } else {
+    result = arr1.every((element, i) => element === arr2[i]);
+  }
 
-  return result; // boolean
+  return result;
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter((item) => item > 0)
+    .filter((item) => item % 3 === 0)
+    .map((item) => item * 10);
 
-  return resultArr; // array
+  return resultArr;
 }
