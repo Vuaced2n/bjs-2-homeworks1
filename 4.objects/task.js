62ffd3b6908e1e3dbf2f6ba18beef30e712dc065
Stefania <stt.orlova@gmail.com>
2021-11-01 21:42:11 +0300

    homework

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+      this.marks = [];
+      this.marks.push(mark);
+  } else {
+      this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...args) {
+  if (this.marks === undefined) {
+      this.marks = [];
+      for (let arg of args) {
+          this.marks.push(arg);
+      }
+  } else {
+      for (let arg of args) {
+          this.marks.push(arg);
+      }
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  let length = this.marks.length;
+  for (let i = 0; i < length; i++) {
+      sum += this.marks[i];
+  }
+  return sum / length;
+}
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+  delete this.marks;
+  delete this.subject;
+}
